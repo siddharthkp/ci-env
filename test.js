2b7afc3b983b361616c5752796587502523ccce1
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-07-13 03:12:45 +0530

    pass tests on non-ci

--- test.js
+++ test.js
@@ -2,46 +2,46 @@ const test = require('ava')
 
 const { repo, sha, event, branch, ci } = require('./index')
 
-if (!ci) {
+if (ci) {
+  console.log('values: ', repo, sha, event, branch, ci)
+
+  test('ci is correctly set', t => {
+    if (process.env.TRAVIS) t.is(ci, 'travis')
+    else if (process.env.CIRCLECI) t.is(ci, 'circle')
+    else if (process.env.WERCKER) t.is(ci, 'wercker')
+  })
+
+  test('repo is correctly set', t => t.is(repo, 'siddharthkp/ci-env'))
+
+  test('sha is set', t => {
+    const real_sha =
+      process.env.TRAVIS_PULL_REQUEST_SHA ||
+      process.env.TRAVIS_COMMIT ||
+      process.env.CIRCLE_SHA1 ||
+      process.env.WERCKER_GIT_COMMIT
+
+    t.is(sha, real_sha)
+  })
+
+  test('event is correctly set', t => {
+    if (ci === 'travis' && process.env.TRAVIS_EVENT_TYPE === 'pull_request')
+      t.is(event, 'pull_request')
+    else t.is(event, 'push')
+  })
+
+  test('branch is correctly set', t => {
+    if (event === 'pull_request')
+      t.is(branch, process.env.TRAVIS_PULL_REQUEST_BRANCH)
+    else {
+      const real_branch =
+        process.env.TRAVIS_BRANCH ||
+        process.env.CIRCLE_BRANCH ||
+        process.env.WERCKER_GIT_BRANCH
+
+      t.is(branch, real_branch)
+    }
+  })
+} else {
   console.log('These tests can only run in CI environments')
-  process.exit(0)
+  test(t => t.pass())
 }
-
-console.log('values: ', repo, sha, event, branch, ci)
-
-test('ci is correctly set', t => {
-  if (process.env.TRAVIS) t.is(ci, 'travis')
-  else if (process.env.CIRCLECI) t.is(ci, 'circle')
-  else if (process.env.WERCKER) t.is(ci, 'wercker')
-})
-
-test('repo is correctly set', t => t.is(repo, 'siddharthkp/ci-env'))
-
-test('sha is set', t => {
-  const real_sha =
-    process.env.TRAVIS_PULL_REQUEST_SHA ||
-    process.env.TRAVIS_COMMIT ||
-    process.env.CIRCLE_SHA1 ||
-    process.env.WERCKER_GIT_COMMIT
-
-  t.is(sha, real_sha)
-})
-
-test('event is correctly set', t => {
-  if (ci === 'travis' && process.env.TRAVIS_EVENT_TYPE === 'pull_request')
-    t.is(event, 'pull_request')
-  else t.is(event, 'push')
-})
-
-test('branch is correctly set', t => {
-  if (event === 'pull_request')
-    t.is(branch, process.env.TRAVIS_PULL_REQUEST_BRANCH)
-  else {
-    const real_branch =
-      process.env.TRAVIS_BRANCH ||
-      process.env.CIRCLE_BRANCH ||
-      process.env.WERCKER_GIT_BRANCH
-
-    t.is(branch, real_branch)
-  }
-})
