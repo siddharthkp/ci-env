1f570837764bf78230907ffbaa99434215317ef6
siddharthkp <siddharth.kshetrapal@gmail.com>
2019-08-14 10:30:01 +0530

    skip test in non-CI environments

--- test.js
+++ test.js
@@ -1,79 +1,109 @@
-const test = require('ava')
+const test = require("ava");
 
-const { buildUrl, jobUrl, repo, sha, event, commit_message, pull_request_number, branch, ci } = require('./index')
+const {
+  buildUrl,
+  jobUrl,
+  repo,
+  sha,
+  event,
+  commit_message,
+  pull_request_number,
+  branch,
+  ci
+} = require("./index");
 
 if (ci) {
-  console.log('values: ', { repo, sha, event, commit_message, pull_request_number, branch, ci })
+  console.log("values: ", {
+    repo,
+    sha,
+    event,
+    commit_message,
+    pull_request_number,
+    branch,
+    ci
+  });
 
-  test('ci is correctly set', t => {
-    if (process.env.TRAVIS) t.is(ci, 'travis')
-    else if (process.env.CIRCLECI) t.is(ci, 'circle')
-    else if (process.env.WERCKER) t.is(ci, 'wercker')
-    else if (process.env.DRONE) t.is(ci, 'drone')
-    else if (process.env.CI_NAME === 'codeship') t.is(ci, 'codeship')
-    else if (process.env.GITHUB_ACTION) t.is(ci, 'github_actions')
-  })
+  test("ci is correctly set", t => {
+    if (process.env.TRAVIS) t.is(ci, "travis");
+    else if (process.env.CIRCLECI) t.is(ci, "circle");
+    else if (process.env.WERCKER) t.is(ci, "wercker");
+    else if (process.env.DRONE) t.is(ci, "drone");
+    else if (process.env.CI_NAME === "codeship") t.is(ci, "codeship");
+    else if (process.env.GITHUB_ACTION) t.is(ci, "github_actions");
+  });
 
-  test('repo is correctly set', t => t.is(repo, 'siddharthkp/ci-env'))
+  test("repo is correctly set", t => t.is(repo, "siddharthkp/ci-env"));
 
-  test('sha is set', t => {
+  test("sha is set", t => {
     const real_sha =
       process.env.TRAVIS_PULL_REQUEST_SHA ||
       process.env.TRAVIS_COMMIT ||
       process.env.CIRCLE_SHA1 ||
       process.env.WERCKER_GIT_COMMIT ||
       process.env.DRONE_COMMIT ||
-      process.env.GITHUB_SHA
+      process.env.GITHUB_SHA;
 
-    t.is(sha, real_sha)
-  })
+    t.is(sha, real_sha);
+  });
 
-  test('commit_message is set', t => {
+  test("commit_message is set", t => {
     const real_commit_message =
       process.env.TRAVIS_COMMIT_MESSAGE ||
       process.env.CI_COMMIT_MESSAGE ||
       process.env.CI_MESSAGE ||
-      ''
+      "";
     // Only travis and codeship set commit message
-    t.is(commit_message, real_commit_message)
-  })
+    t.is(commit_message, real_commit_message);
+  });
 
-  test('pull_request_number is set', t => {
-    let circlePullRequestNumber
+  test("pull_request_number is set", t => {
+    let circlePullRequestNumber;
     if (process.env.CI_PULL_REQUEST)
-      circlePullRequestNumber = process.env.CI_PULL_REQUEST.split('/').pop()
+      circlePullRequestNumber = process.env.CI_PULL_REQUEST.split("/").pop();
 
     const real_pull_request_number =
       process.env.TRAVIS_PULL_REQUEST ||
       process.env.DRONE_PULL_REQUEST ||
       circlePullRequestNumber ||
-      '' // wercker does not expose pull request number
+      ""; // wercker does not expose pull request number
 
-    t.is(pull_request_number, real_pull_request_number)
-  })
+    t.is(pull_request_number, real_pull_request_number);
+  });
 
-  test('jobUrl is set', t => {
-    let real_jobUrl
-    if (process.env.TRAVIS) real_jobUrl = `https://travis-ci.org/${repo}/jobs/${process.env.TRAVIS_JOB_ID}`
-    t.is(jobUrl, real_jobUrl)
-  })
+  test("jobUrl is set", t => {
+    let real_jobUrl;
+    if (process.env.TRAVIS)
+      real_jobUrl = `https://travis-ci.org/${repo}/jobs/${
+        process.env.TRAVIS_JOB_ID
+      }`;
+    t.is(jobUrl, real_jobUrl);
+  });
 
-  test('buildUrl is set', t => {
-    let real_buildUrl
-    if (process.env.TRAVIS) real_buildUrl = `https://travis-ci.org/${repo}/builds/${process.env.TRAVIS_JOB_ID}`
-    t.is(buildUrl, real_buildUrl)
-  })
+  test("buildUrl is set", t => {
+    let real_buildUrl;
+    if (process.env.TRAVIS)
+      real_buildUrl = `https://travis-ci.org/${repo}/builds/${
+        process.env.TRAVIS_JOB_ID
+      }`;
+    t.is(buildUrl, real_buildUrl);
+  });
 
-  test('event is correctly set', t => {
-    if ((ci === 'travis' && process.env.TRAVIS_EVENT_TYPE === 'pull_request') ||
-        (ci === 'github_actions' && process.env.GITHUB_EVENT_NAME === 'pull_request'))
-      t.is(event, 'pull_request')
-    else t.is(event, 'push')
-  })
+  test("event is correctly set", t => {
+    if (
+      (ci === "travis" && process.env.TRAVIS_EVENT_TYPE === "pull_request") ||
+      (ci === "github_actions" &&
+        process.env.GITHUB_EVENT_NAME === "pull_request")
+    )
+      t.is(event, "pull_request");
+    else t.is(event, "push");
+  });
 
-  test('branch is correctly set', t => {
-    if (event === 'pull_request') t.is(branch, process.env.TRAVIS_PULL_REQUEST_BRANCH ||
-                                       process.env.GITHUB_REF)
+  test("branch is correctly set", t => {
+    if (event === "pull_request")
+      t.is(
+        branch,
+        process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.GITHUB_REF
+      );
     else {
       const real_branch =
         process.env.TRAVIS_BRANCH ||
@@ -81,12 +111,11 @@ if (ci) {
         process.env.WERCKER_GIT_BRANCH ||
         process.env.DRONE_BRANCH ||
         process.env.CI_BRANCH || // codeship
-        process.env.GITHUB_REF
+        process.env.GITHUB_REF;
 
-      t.is(branch, real_branch)
+      t.is(branch, real_branch);
     }
-  })
+  });
 } else {
-  console.log('These tests can only run in CI environments')
-  test(t => t.pass())
+  test.skip("These tests can only run in CI environments", t => t.pass());
 }
